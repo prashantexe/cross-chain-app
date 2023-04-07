d1e38df1fb08e305abfdacb41085fc5454f62f95
Prashant <prashantd049@gmail.com>
2023-04-07 09:01:49 +0530

    Updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="w-screen h-screen bg-gray-300 flex flex-col items-center justify-center gap-8 text-black">
+      <h1 className="text-6xl font-bold">Cross chain swap!</h1></div>
   )
 }
